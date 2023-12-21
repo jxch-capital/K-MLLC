a947e0da639bcf77822c818baaad4355ef0b70dd
jxch <qianbuhan@proton.me>
2023-12-22 02:41:18 +0800

    添加常用特征计算

---
+++ features.py
@@ -0,0 +1,39 @@
+import ta.wrapper
+import numpy as np
+
+
+class HLC3:
+    @staticmethod
+    def hlc3_hlc(high, low, close):
+        return (high + low + close) / 3
+
+    @staticmethod
+    def hlc3_lc(high, low, close):
+        return (high + 2 * low + 2 * close) / 5
+
+    @staticmethod
+    def hlc3_c(high, low, close):
+        return (high + low + 2 * close) / 4
+
+
+def n_rsi(price, rsi_n, rsi_sma_n):
+    rsi_values = ta.wrapper.RSIIndicator(close=price, window=rsi_n).rsi()
+    return ta.wrapper.SMAIndicator(rsi_values, window=rsi_sma_n).sma_indicator()
+
+
+def n_cci(price, cci_n, cci_sma_n):
+    cci_values = ta.wrapper.CCIIndicator(high=price, low=price, close=price, window=cci_n).cci()
+    return ta.wrapper.SMAIndicator(cci_values, window=cci_sma_n).sma_indicator()
+
+
+def n_adx(high, low, close, adx_n):
+    return ta.wrapper.ADXIndicator(high=high, low=low, close=close, window=adx_n).adx()
+
+
+def n_wt(high, low, close, rsi_n, wt_sma_n, hlc3_func=HLC3.hlc3_hlc):
+    hlc3 = hlc3_func(high, low, close)
+    rsi = ta.wrapper.RSIIndicator(close=hlc3, window=rsi_n).rsi()
+    rsi_smooth = ta.wrapper.SMAIndicator(close=rsi, window=wt_sma_n).sma_indicator()
+    rsi_double_smooth = ta.wrapper.SMAIndicator(close=rsi_smooth, window=wt_sma_n).sma_indicator()
+    wt = rsi_double_smooth - rsi_smooth
+    return ta.wrapper.SMAIndicator(close=wt, window=wt_sma_n).sma_indicator()
--- proxy.py
+++ proxy.py
@@ -3,5 +3,5 @@ import socks
 
 
 def enable(host="localhost", port=10808):
-    socks.setdefaultproxy(socks.SOCKS5, "localhost", 10808)
+    socks.setdefaultproxy(socks.SOCKS5, host, port)
     socket.socket = socks.socksocket
